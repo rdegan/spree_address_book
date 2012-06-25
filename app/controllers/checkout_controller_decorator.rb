d5ddaa22002890e011aab0b971274e4f369273e3
Riccardo Degan <riccardo.degan@diginess.it>
2012-06-25 18:46:47 +0200

    Fix bug with clone.

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -3,8 +3,16 @@ CheckoutController.class_eval do
 
   def before_address
     if current_user
-      @order.bill_address ||= current_user.bill_address || Address.default
-      @order.ship_address ||= current_user.ship_address || Address.default
+      if current_user.ship_address
+        @order.bill_address ||= current_user.ship_address
+      else
+        @order.bill_address ||= Address.default
+      end
+      if current_user.bill_address
+        @order.ship_address ||= current_user.bill_address
+      else
+        @order.ship_address ||= Address.default
+      end
     else
       @order.bill_address ||= Address.default
       @order.ship_address ||= Address.default
@@ -14,18 +22,16 @@ CheckoutController.class_eval do
   def update_user_address
 
     if current_user
-    unless params[:remember_bill_address].blank?
-      current_user.bill_address.delete unless current_user.bill_address.blank?
-      current_user.bill_address = @order.bill_address.clone
-    end
+      unless params[:remember_bill_address].blank?
+        current_user.bill_address = @order.bill_address
+      end
 
-    unless params[:remember_ship_address].blank?
-          current_user.ship_address.delete unless current_user.ship_address.blank?
-          current_user.ship_address = @order.ship_address.clone
-    end
-    current_user.save
+      unless params[:remember_ship_address].blank?
+        current_user.ship_address = @order.ship_address
+      end
+      current_user.save
 
     end
-    end
+  end
 
 end
