a72a299ecaaa23bc48e614e7f05843eea02d0906
Riccardo Degan <riccardo.degan@diginess.it>
2012-08-06 08:51:46 +0200

    Fix namespace forgot

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -5,7 +5,7 @@ Spree::UsersController.class_eval do
     if @user.update_with_password(params)
       if params[:user][:password].present?
         # this logic needed b/c devise wants to log us out after password changes
-        user = User.reset_password_by_token(params[:user])
+        user = Spree::User.reset_password_by_token(params[:user])
         sign_in(@user, :event => :authentication, :bypass => !Spree::Auth::Config[:signout_after_password_change])
       end
       flash.notice = I18n.t("account_updated")
