ce769be41a384ed53872c30190b027bb0b238195
Damiano <damiano.giacomello@diginess.it>
2013-02-22 10:39:31 +0100

    fixed checkout registration and before_address overrides

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,16 +1,22 @@
 Spree::CheckoutController.class_eval do
   after_filter :update_user_address, :only => :update
-
+  
+  def registration
+    @user = Spree::User.new
+    @user.bill_address ||= Spree::Address.default
+    @user.ship_address ||= Spree::Address.default
+  end
+  
   def before_address
     if current_user
-      @order.bill_address ||= current_user.bill_address || Address.default
-      @order.ship_address ||= current_user.ship_address || Address.default
+      @order.bill_address ||= current_user.bill_address || Spree::Address.default
+      @order.ship_address ||= current_user.ship_address || Spree::Address.default
     else
-      @order.bill_address ||= Address.default
-      @order.ship_address ||= Address.default
+      @order.bill_address ||= Spree::Address.default
+      @order.ship_address ||= Spree::Address.default
     end
   end
-
+  
   def update_user_address
     unless params[:remember_bill_address].blank?
       current_user.bill_address = @order.bill_address
