7162136bab93239bbfa57d5ea60c2e727e6e195f
Damiano <damiano.giacomello@diginess.it>
2013-02-26 17:22:10 +0100

    removed validate uniqueness for tax_id_number and added condition for validate length.

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,9 +1,8 @@
 Spree::Address.class_eval do
   attr_accessible :tax_id_number, :vat_number
   
-  validates :vat_number, :length => {:maximum => 11}
-  validates :tax_id_number, :length => {:in => 11..16}
-  validates_uniqueness_of :tax_id_number
+  validates :vat_number, :length => {:maximum => 11}, :if => Proc.new {|a| !a.vat_number.blank? }
+  validates :tax_id_number, :length => {:in => 11..16}, :if => Proc.new {|a| !a.tax_id_number.blank? }
   
   def empty?
     attributes.except('id', 'created_at', 'updated_at', 'order_id', 'country_id', 'tax_id_number', 'vat_number').all? { |_, v| v.nil? }
