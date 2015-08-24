40a5d3fcd19d7ef1d8ab922922f757ae3e8e1c9f
Damiano Diginess <damiano.giacomello@diginess.it>
2015-08-24 14:59:08 +0200

    remove after_filter obsolete

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,33 +1,29 @@
 Spree::CheckoutController.class_eval do
-  after_filter :update_user_address, :only => :update
-  
-  def registration
-    @user = Spree::User.new
-    country = Spree::Country.find(Spree::Config[:default_country_id]) rescue Spree::Country.first
-    @user.build_bill_address({:country => country}, :without_protection => true) if !@user.bill_address
-    @user.build_ship_address({:country => country}, :without_protection => true) if !@user.ship_address
-  end
-  
-  def before_address
-    if current_user
-      @order.bill_address ||= current_user.bill_address || Spree::Address.default
-      @order.ship_address ||= current_user.ship_address || Spree::Address.default
-    else
-      @order.bill_address ||= Spree::Address.default
-      @order.ship_address ||= Spree::Address.default
-    end
-  end
-  
-  def update_user_address
-    unless params[:remember_bill_address].blank?
-      current_user.bill_address = @order.bill_address
-    end
+	def registration
+		@user = Spree::User.new
+		country = Spree::Country.find(Spree::Config[:default_country_id]) rescue Spree::Country.first
+		@user.build_bill_address({:country => country}, :without_protection => true) if !@user.bill_address
+		@user.build_ship_address({:country => country}, :without_protection => true) if !@user.ship_address
+	end
 
-    unless params[:remember_ship_address].blank?
-          current_user.ship_address = @order.ship_address
-    end
-    current_user.save
+	def before_address
+		if current_user
+			@order.bill_address ||= current_user.bill_address || Spree::Address.default
+			@order.ship_address ||= current_user.ship_address || Spree::Address.default
+		else
+			@order.bill_address ||= Spree::Address.default
+			@order.ship_address ||= Spree::Address.default
+		end
+	end
+  
+	def update_user_address
+		unless params[:remember_bill_address].blank?
+			current_user.bill_address = @order.bill_address
+		end
 
-  end
-
+		unless params[:remember_ship_address].blank?
+			current_user.ship_address = @order.ship_address
+		end
+		current_user.save
+	end
 end
